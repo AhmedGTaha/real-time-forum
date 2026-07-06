778d5c39c43f2388d9170fd2eb1ed68c653ffccd
Ahmed Taha <ahmedtahaghodhbani@gmail.com>
2026-07-06 11:52:41 +0300

    Add WebSocket hub to app

--- handlers/app.go
+++ handlers/app.go
@@ -3,12 +3,14 @@ package handlers
 import "database/sql"
 
 type App struct {
-	DB *sql.DB
+	DB  *sql.DB
+	Hub *Hub
 }
 
 // This function receives the database connection from main.go and returns an App
 func NewApp(db *sql.DB) *App {
 	return &App{
-		DB: db,
+		DB:  db,
+		Hub: NewHub(),
 	}
 }
